8163df704b47ddf207c23e28f3ecf8f139140bb4
Syed-Suhaan <suhaansyed2004@gmail.com>
2025-10-21 20:07:55 +0530

    Added Listening and Accepting Client Code

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Testing Github setup")
+	listener, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		log.Fatalf("Failed to Start server :%v", err)
+	}
+	fmt.Println("Radish server started on port 6379")
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Println("Failed to accept the connection:", err)
+		} else {
+			fmt.Println("New client connected!")
+		}
+		connection.Close()
+	}
 }
